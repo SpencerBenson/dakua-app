4b759297175a1ba411d432cc65d388a9a45449fc
Benson Njuguna K <bennspencer@gmail.com>
2020-08-24 01:29:04 +0300

    profile page design

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,9 +1,151 @@
-import React from 'react'
+import React from "react";
+import { Row, Col } from "antd";
+const Profile = () => {
+  return (
+    // <div className="profile-page" style={{
+    //   maxWidth:"550px",
+    //   margin:"0px auto"
+    // }}>
+    <Row>
+      <Col span={18} offset={3}>
+        <Row justify="center" style={{ fontSize: "15px", margin: "20px 0px" }}>
+          <Col xs={24} xl={4}>
+            <img
+              style={{ width: "7em", height: "7em", borderRadius: "50%" }}
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={0} xl={1}>
+            &nbsp;
+          </Col>
 
-const Profile = ()=>{
-  return(
-    <h1>Profile</h1>
-  )
-}
+          <Col xs={24} xl={8} style={{textAlign:"left"}}>
+            <h1>Spencer Benson</h1>
+            <div className="followings"
+              style={{
+                display: "flex",
+                justifyContent: "left",
+                paddingRight:"1em",
+                width: "100%",
+              }}
+            >
+              <p><span><b>50</b></span> posts </p>
+              <p><span><b>135</b></span> followers </p>
+              <p><span><b>1398</b></span> following </p>
+            </div>
+          </Col>
+        </Row>
 
+        <Row>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+          <Col xs={24} xl={8}>
+            <img
+              className="item"
+              src="https://images.unsplash.com/photo-1516350543-b12209cfa029?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=60"
+              alt="person"
+            />
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
+};
 export default Profile;
