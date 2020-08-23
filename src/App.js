c3ab5082d0a3b9e4995c9d6e412074dc53f4244d
Benson Njuguna K <bennspencer@gmail.com>
2020-08-23 17:16:03 +0300

    added antd layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import './App.css';
-import NavBar from './components/Navbar'
-import {BrowserRouter,Route} from 'react-router-dom'
-import Home from './components/screens/Home'
-import Login from './components/screens/Login'
-import Profile from './components/screens/Profile'
-import Signup from './components/screens/Signup'
-
+import React from "react";
+// import './App.css';
+import NavBar from "./components/Navbar";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/screens/Home";
+import Login from "./components/screens/Login";
+import Profile from "./components/screens/Profile";
+import Signup from "./components/screens/Signup";
+import "./App.less";
+import { Layout } from "antd";
+const {  Content } = Layout;
 
 function App() {
   return (
-   <BrowserRouter>
-      <NavBar />
-      <Route path='/' exact>
-        <Home />
-      </Route>
-      <Route path='/login'>
-        <Login />
-      </Route>
-      <Route path='/signup'>
-        <Signup />
-      </Route>
-      <Route path='/profile'>
-        <Profile />
-      </Route>
-   </BrowserRouter>
+    <BrowserRouter>
+    <>
+    <NavBar />
+      <Layout>
+        <Content>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Content>
+      </Layout>
+      </>
+    </BrowserRouter>
   );
 }
 
