edfb74b6b5bf80c89fdc956487131fc34d977336
Benson Njuguna K <bennspencer@gmail.com>
2020-08-23 17:14:05 +0300

    Signup page created with validationnn

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,9 +1,98 @@
-import React from 'react'
+import React from "react";
+import { Form, Input, Button, Checkbox, Card, Row, Col } from "antd";
+import {Link} from 'react-router-dom';
 
-const Signup = ()=>{
-  return(
-    <h1>Signup</h1>
-  )
-}
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
 
+const Signup = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Row>
+      <Col span={12} offset={6}>
+        <Card  style={{margin: 30}}>
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+          <h1 style={{textAlign:'center'}}>Login</h1>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your name!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}  style={{textAlign: 'center'}}>
+              <Button type="primary" htmlType="submit"  size='large'>
+              Sign up
+        </Button>
+            </Form.Item>
+           <Link to="/login">Already have an account?</Link>
+           
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
 export default Signup;
