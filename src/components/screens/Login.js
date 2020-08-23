abc8534ca96c0e54dc273681e24f34bf3720d3aa
Benson Njuguna K <bennspencer@gmail.com>
2020-08-23 17:14:22 +0300

    login page created with validation

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,19 +1,87 @@
 import React from "react";
+import { Form, Input, Button, Checkbox, Card, Row, Col } from "antd";
+import {Link} from 'react-router-dom';
 
-const Login = () => {
-  return (
-    <>
-      <div class="card">
-        <h2>Dakua</h2>
-        
-        <input type="text" placeholder='email'
-        />
-        <input type="password" placeholder='password'
-        />
-        <button class="btn waves-effect waves-light">Submit</button>
-      </div>
-    </>
-  );
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
 };
 
+const Login = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Row>
+      <Col span={12} offset={6}>
+        <Card  style={{margin: 30}}>
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          >
+
+<h1 style={{textAlign:'center'}}>Login</h1>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout} style={{textAlign: 'center'}}>
+              <Button type="primary" htmlType="submit"  size='large'>
+                Login
+              </Button>
+            </Form.Item>
+
+           <Link to="/signup">Don't have an account?</Link>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
 export default Login;
